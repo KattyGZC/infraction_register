088e135616b4a59e5d25651730874e6b56456343
KattyGZC <kattyko.gzc@gmail.com>
2024-07-06 00:52:38 -0500

    Creación de nuevos registros

--- register_app/urls.py
+++ register_app/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import PersonListView
+from .views import (PersonListView, PersonCreateView, 
+                    VehicleListView, VehicleCreateView, 
+                    OfficerListView, OfficerCreateView)
 
 app_name = 'register_app'
 
 urlpatterns = [
     path("person", PersonListView.as_view(), name="person_list"),
-    path("vehicle", PersonListView.as_view(), name="vehicle_list"),
-    path("officer", PersonListView.as_view(), name="officer_list"),
+    path("person/create", PersonCreateView.as_view(), name="person_create"),
+    path("vehicle", VehicleListView.as_view(), name="vehicle_list"),
+    path("vehicle/create", VehicleCreateView.as_view(), name="vehicle_create"),
+    path("officer", OfficerListView.as_view(), name="officer_list"),
+    path("officer/create", OfficerCreateView.as_view(), name="officer_create"),
 ]
--- register_app/views.py
+++ register_app/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 from .models import Person, Vehicle, Officers, Infraction
@@ -9,6 +10,7 @@ class PersonListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = 'Listado de Personas'
+        context['url_create'] = reverse('register_app:person_create')
         return context
     
 
@@ -19,6 +21,7 @@ class VehicleListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = 'Listado de Vehículos'
+        context['url_create'] = reverse('register_app:vehicle_create')
         return context
 
 
@@ -29,4 +32,43 @@ class OfficerListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = 'Listado de Oficiales'
-        return context    
+        context['url_create'] = reverse('register_app:officer_create')
+        return context  
+
+class PersonCreateView(CreateView):
+    model = Person
+    fields = '__all__'
+    template_name = 'register_app/form.html'
+    success_url = reverse_lazy('register_app:person_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Nuevo registro de Persona'
+        context["url"] = reverse('register_app:person_list')
+        return context 
+
+
+class VehicleCreateView(CreateView):
+    model = Vehicle
+    fields = '__all__'
+    template_name = 'register_app/form.html'
+    success_url = reverse_lazy('register_app:vehicle_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Nuevo registro de vehículo'
+        context["url"] = reverse('register_app:vehicle_list')
+        return context 
+    
+
+class OfficerCreateView(CreateView):
+    model = Officers
+    fields = '__all__'
+    template_name = 'register_app/form.html'
+    success_url = reverse_lazy('register_app:officer_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Nuevo registro de Oficial'
+        context["url"] = reverse('register_app:officer_list')
+        return context 
